e2b0f7cb4d526b92b98af8fa87bd7fe406d5d6a1
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-04-06 08:18:06 -0300

    apenas finalizando explicações sobre o lazy, agora como grid

--- app/src/main/java/com/aulasandroid/lista_lazy/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/lista_lazy/MainActivity.kt
@@ -14,6 +14,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -98,15 +101,22 @@ fun GamesScreen(modifier: Modifier = Modifier) {
             }
         )
 
-        LazyRow() {
+//        LazyRow() {
+//            items(gamesList){
+//                StudioCard(it)
+//            }
+//        }
+//
+//        LazyColumn() {
+//            items(gamesList) {
+//                GameCard(it)
+//            }
+//        }
+
+        LazyVerticalGrid(columns = GridCells.Adaptive(150.dp)) {
             items(gamesList){
                 StudioCard(it)
-            }
-        }
 
-        LazyColumn() {
-            items(gamesList) {
-                GameCard(it)
             }
         }
     }
